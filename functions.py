74162dd79a8dca93e13880df3898c61746d53b14
mkniz <kniz.michal@gmail.com>
2023-07-09 15:23:03 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items """
+    with open(filepath, "r") as f:
+        todos_local = f.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file """
+    with open(filepath, "w") as f:
+        f.writelines(todos_arg)
